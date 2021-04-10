5b4dd4d8a658d8d01d9df64f680cae98f99090a1
rococo <rococo@tangleroad.com>
2021-04-09 22:06:49 -0700

    add -y

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 yum update -y
-amazon-linux-extras install docker
+amazon-linux-extras install docker -y
 service docker start
 usermod -a -G docker ec2-user
 echo Setup complete. Relog and run docker info to verify installation worked.
